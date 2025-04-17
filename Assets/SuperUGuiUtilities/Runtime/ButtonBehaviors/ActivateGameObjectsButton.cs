5b2f674dc9045a5be1a810e13ff55e08818dd805
JoshDawkins <JD_techsupport@hotmail.com>
2025-04-17 06:05:50 -0500

    Change Samples Layout
    -Changed the samples to be on 4 separate screens with a selection screen to move between them, to better fit a smaller viewing area for the web

---
+++ Assets/SuperUGuiUtilities/Runtime/ButtonBehaviors/ActivateGameObjectsButton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace SuperUGuiUtilities {
+	public class ActivateGameObjectsButton : ButtonBehavior {
+		[SerializeField]
+		private Config[] targets;
+
+		protected override void OnClick() => targets.ForEach(t => t.SetActive());
+
+
+		[Serializable]
+		private struct Config {
+			[SerializeField]
+			private GameObject target;
+			[SerializeField]
+			private bool activate;
+
+			public void SetActive() => target.TrySetActive(activate);
+		}
+	}
+}
